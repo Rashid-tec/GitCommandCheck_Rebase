d68bca3e3bf64e46cb984edce7e658a305737fdf
Rashid_khan <khan.rashid059@gmail.com>
2026-04-05 13:27:05 +0530

    first commit

---
+++ src/MathmaticallOperation.java
@@ -0,0 +1,14 @@
+
+public class MathmaticallOperation {
+	
+	public static void main(String[] args) {
+		MathmaticallOperation s=new MathmaticallOperation();
+		s.add(4, 6);
+	}
+	public void add(int a,int b)
+	{
+		int c=a+b;
+		System.out.println(c);
+	}
+
+}
