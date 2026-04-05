1d3ed2f6c1636d3fac2f75900d1e58ee9b8e2c84
Rashid_khan <khan.rashid059@gmail.com>
2026-04-05 13:52:26 +0530

    Added subtract method

--- src/MathmaticallOperation.java
+++ src/MathmaticallOperation.java
@@ -4,11 +4,15 @@ public class MathmaticallOperation {
 	public static void main(String[] args) {
 		MathmaticallOperation s=new MathmaticallOperation();
 		s.add(4, 6);
+		System.out.println(s.subtract(9,3));
 	}
 	public void add(int a,int b)
 	{
 		int c=a+b;
 		System.out.println(c);
+	}
+	public int subtract(int a, int b){
+	    return a - b;
 	}
 
 }
